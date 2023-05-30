1f0a8e228dff60d4d7af9286493b8dcdd1f8bef4
akshaybannatti <50884750+akshaybannatti@users.noreply.github.com>
2023-05-31 01:53:42 +0530

    Update about.py

--- Final_Build/pyidm/about.py
+++ Final_Build/pyidm/about.py
@@ -1,12 +1,12 @@
-about_notes = f"""This program has been written in Python to increase download speed as well as to complete the process
-of downloading with ease by providing Pause-Resume capability. We convey our sincere thanks to our
+about_notes = This program has been written in Python to increase download speed as well as to complete the process
+of downloading with ease by providing Pause-Resume capability. I convey my sincere thanks to my
 Project Guide Mr J. Albert Mayan for helping us guide our path leading to right direction for the
 development of this program. We then thank our institution -- Sathyabama Institute of Science and
 Technology for enlightening us with knowledge & providing us a platform to perform.
 
 
 Developers,
-Akash Dubey & Akshay Kumar
+Akshay Bannatti
 2019-2020 Batch
 CSE Department
 Sathyabama Institute of Science & Technology
